2ebf018455b4b09cb898571bed4c97ed4a93559a
joshuavaron <32440072+joshtheclasher@users.noreply.github.com>
2022-05-27 05:11:22 -0700

    0.2.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='euclipy',
-  version='0.2.5',
+  version='0.2.6',
   description='A Euclidean Geometry based library',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
