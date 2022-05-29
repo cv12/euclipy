4332b0fc3abcc9a72e9f45e95673fce7eb44e19a
joshuavaron <32440072+joshtheclasher@users.noreply.github.com>
2022-05-29 14:36:39 -0700

    Added unit tests

---
+++ tests/test_advanced_symbolic_geometry.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../')
+import pytest
+
+from euclipy.core import *
+from euclipy.measure import *
+from euclipy.polygon import *
+from euclipy.theorems import *
+from euclipy.tools import *
+from euclipy.registry import *
